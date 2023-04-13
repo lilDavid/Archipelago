289c77066fb37118abe879f2dd2ddd002dc0a478
lil David <1337lilDavid@gmail.com>
2023-04-13 10:54:49 -0500

    Re-add WL4 launcher component

--- worlds/LauncherComponents.py
+++ worlds/LauncherComponents.py
@@ -92,6 +92,9 @@ components: List[Component] = [
     Component('Starcraft 2 Client', 'Starcraft2Client'),
     # Wargroove
     Component('Wargroove Client', 'WargrooveClient'),
+    # Wario Land 4
+    Component('Wario Land 4 Client', 'WL4Client',
+              file_identifier=SuffixIdentifier('.apwl4')),
     # Zillion
     Component('Zillion Client', 'ZillionClient',
               file_identifier=SuffixIdentifier('.apzl')),

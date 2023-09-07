492d3b608a938a8ba7ef90f4858b4487fd4433c0
lilDavid <1337lilDavid@gmail.com>
2023-09-07 09:34:26 -0500

    WL4: Inject client launcher component

--- WL4Client.py
+++ WL4Client.py
@@ -1,5 +1,3 @@
-import asyncio
-import colorama
 import Utils
 
 from worlds.wl4.client import launch
@@ -7,6 +5,4 @@ from worlds.wl4.client import launch
 
 if __name__ == '__main__':
     Utils.init_logging('WL4Client')
-    colorama.init()
-    asyncio.run(launch())
-    colorama.deinit()
+    launch()
